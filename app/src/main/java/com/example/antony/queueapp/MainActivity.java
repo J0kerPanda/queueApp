4ee8c0b5dc3bce1a69c43ec08ac1adc89f5f6c8c
Semenov <anton.semenov@metahouse.ru>
2018-05-06 17:10:31 +0300

    Added basic calendar view

--- app/src/main/java/com/example/antony/queueapp/MainActivity.java
+++ app/src/main/java/com/example/antony/queueapp/MainActivity.java
@@ -13,30 +13,23 @@ import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        textView = findViewById(R.id.mainText);
     }
 
-    public void mainButtonHandler(View v) {
-        textView.setText("started");
+    public void scheduleButtonHandler(View v) {
         Log.d("MY_CUSTOM_LOG", "started");
         ApiHttpClient.get("/user/1", new RequestParams(), new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
                 Log.d("MY_CUSTOM_LOG", "ok");
-                textView.setText(new String(responseBody));
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
                 Log.d("MY_CUSTOM_LOG", "fail");
-                textView.setText(error.getMessage());
             }
         });
     }
