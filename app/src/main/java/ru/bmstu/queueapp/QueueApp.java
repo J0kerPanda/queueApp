6fd82e5fb59395265931356946cb9a7466c5db59
Антон Семенов <anton.semenov@metahouse.ru>
2018-05-27 18:24:49 +0300

    Added saving user to shared preference

--- app/src/main/java/ru/bmstu/queueapp/QueueApp.java
+++ app/src/main/java/ru/bmstu/queueapp/QueueApp.java
@@ -2,17 +2,28 @@ package ru.bmstu.queueapp;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 
 import ru.bmstu.queueapp.http.data.UserData;
 
 public class QueueApp extends Application {
 
+    private static final String userIdKey = "userId";
+    private static final String userEmailKey = "email";
+    private static final String userFirstNameKey = "firstName";
+    private static final String userSurnameKey = "surname";
+    private static final String userPatronymicKey = "patronymic";
+    private static final String userIsHostKey = "isHost";
+
     private static Application app;
     private static UserData user;
+    private static SharedPreferences sharedPreferences;
 
     public void onCreate() {
         super.onCreate();
-        app = this;
+        QueueApp.app = this;
+        QueueApp.sharedPreferences = getSharedPreferences(getString(R.string.shared_preferences), Context.MODE_PRIVATE);
+        loadUser();
     }
 
     public static Context getAppContext() {
@@ -21,9 +32,39 @@ public class QueueApp extends Application {
 
     public static synchronized void setUser(UserData user) {
         QueueApp.user = user;
+        saveUser(user);
     }
 
     public static UserData getUser() {
+        if (QueueApp.user == null) {
+            loadUser();
+        }
         return QueueApp.user;
+    }
+
+    private static void loadUser() {
+        if (sharedPreferences != null) {
+            if (sharedPreferences.getInt(userIdKey, -1 ) > 0) {
+                setUser(new UserData(
+                    sharedPreferences.getInt(userIdKey, -1),
+                    sharedPreferences.getString(userEmailKey, ""),
+                    sharedPreferences.getString(userFirstNameKey, ""),
+                    sharedPreferences.getString(userSurnameKey, ""),
+                    sharedPreferences.getString(userPatronymicKey, ""),
+                    sharedPreferences.getBoolean(userIsHostKey, false)
+                ));
+            }
+        }
+    }
+
+    private static void saveUser(UserData user) {
+        SharedPreferences.Editor edit = sharedPreferences.edit();
+        edit.putInt(userIdKey, user.id);
+        edit.putString(userEmailKey, user.email);
+        edit.putString(userFirstNameKey, user.firstName);
+        edit.putString(userSurnameKey, user.surname);
+        edit.putString(userPatronymicKey, user.patronymic);
+        edit.putBoolean(userIsHostKey, user.isHost);
+        edit.apply();
     }
 }
--- app/src/main/java/ru/bmstu/queueapp/http/data/UserData.java
+++ app/src/main/java/ru/bmstu/queueapp/http/data/UserData.java
@@ -11,8 +11,30 @@ public class UserData implements Serializable {
     public String patronymic;
     public boolean isHost;
 
+    public UserData() {}
+
+    public UserData(int id, String email, String firstName, String surname, String patronymic, boolean isHost) {
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.patronymic = patronymic;
+        this.isHost = isHost;
+    }
+
     public String fullName() {
         return String.format("%s %s. %s.", surname, firstName.charAt(0), patronymic.charAt(0));
+    }
+
+    public String fullData() {
+        return "UserData{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", surname='" + surname + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", isHost=" + isHost +
+                '}';
     }
 
     //spinner display
